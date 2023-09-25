d03b19b60093629cad1e356a83f953983a685a4e
Ervin Bara <ervinbara17@gmail.com>
2023-09-25 19:38:30 +0200

    Add real content

--- js/skills-modal-effect.js
+++ js/skills-modal-effect.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const skills = [
-        { name: 'php10', level: 100, description: 'Description of your experience with PHP coco.', color: '#777bb3' },
-        { name: 'symfony', level: 80, description: 'Description of your experience with caca.', color: '#1a171b' },
-        { name: 'html3', level: 75, description: 'Description of your experience with PHP.', color: '#e44f26' },
-        { name: 'css3', level: 75, description: 'Description of your experience with PHP.', color: '#1572b6' },
-        { name: 'python1', level: 75, description: 'Description of your experience with PHP.', color: '#3776ab' },
-        { name: 'javascript17', level: 75, description: 'Description of your experience with PHP.', color: '#f7df1e' },
-        { name: 'bootstrap4', level: 75, description: 'Description of your experience with PHP.', color: '#563d7c' },
-        { name: 'mysql', level: 75, description: 'Description of your experience with PHP.', color: '#cbc6d1' },
-        { name: 'responsive2', level: 75, description: 'Description of your experience with PHP.', color: '#cbc6d1' },
-        { name: 'github18', level: 75, description: 'Description of your experience with PHP.', color: '#f5f5f5' },
+        { name: 'php10', level: 85, description: 'J\'ai acquis une solide expérience en PHP au cours de mes études ces dernières années. J\'ai eu l\'opportunité de travailler sur divers aspects du langage, de la programmation orientée objet à la manipulation avancée des bases de données. J\'ai ainsi pu mettre en place plusieurs sites web, renforçant ainsi ma maîtrise de ce langage essentiel. De plus, je suis constamment en veille technologique pour rester à jour avec les dernières versions de PHP et les meilleures pratiques de développement. Cette démarche me permet d\'intégrer les dernières avancées et d\'optimiser mes projets pour des performances optimales.', color: '#777bb3' },
+        { name: 'symfony', level: 80, description: 'Pendant mes études, j\'ai eu l\'occasion de travailler en profondeur avec le framework Symfony. J\'ai développé des applications web robustes et performantes en utilisant les fonctionnalités avancées de ce framework. Cela m\'a permis de comprendre l\'architecture MVC et de maîtriser les bonnes pratiques de développement Symfony. De plus, je reste constamment à l\'affût des dernières nouveautés et tendances dans l\'écosystème Symfony. Je teste régulièrement de nouveaux bundles et fonctionnalités pour rester à jour avec les meilleures pratiques de développement', color: '#1a171b' },
+        { name: 'html3', level: 90, description: 'J\'ai acquis une solide expérience en HTML au fil des années, depuis mes premiers pas dans la programmation.', color: '#e44f26' },
+        { name: 'css3', level: 75, description: 'J\'ai acquis une solide expérience en CSS au fil des années, depuis mes premiers pas dans la programmation.', color: '#1572b6' },
+        { name: 'python1', level: 60, description: 'J\'ai utilisé Python dans divers projets, allant de la création d\'un mini-jeu pour PC à la conception d\'une application interne d\'entreprise dédiée à la collecte d\'informations.', color: '#3776ab' },
+        { name: 'javascript17', level: 70, description: 'Javascript est un langage que j\'utilise régulièrement depuis plusieurs années, notamment dans divers projets où il a été essentiel pour ajouter des fonctionnalités interactives et dynamiques', color: '#f7df1e' },
+        { name: 'bootstrap4', level: 75, description: 'J\'ai souvent eu recours à Bootstrap dans mes projets pour gagner du temps dans le développement de l\'interface utilisateur. Cela m\'a permis de créer des designs réactifs et esthétiques.', color: '#563d7c' },
+        { name: 'mysql', level: 80, description: 'J\'utilise MySQL depuis plusieurs années, j\'ai également suivi des formations spécifiques pour approfondir mes connaissances dans ce domaine. Cela m\'a permis de beaucoup mieux comprendre la gestion de bases de données et d\'optimiser les requêtes pour de meilleurs performances.', color: '#cbc6d1' },
+        { name: 'responsive2', level: 75, description: 'Le Responsive Design est essentiel dans mes projets, assurant une expérience utilisateur optimale sur tous les appareils.', color: '#cbc6d1' },
+        { name: 'github18', level: 80, description: 'GitHub est une plateforme que j\'utilise régulièrement pour héberger mes projets et collaborer avec d\'autres développeurs. J\'y partage mes codes source, découvre des projets open source intéressants et contribue à des communautés de développeurs. C\'est un outil incontournable dans mon parcours en tant que développeur.', color: '#f5f5f5' },
         // Ajoutez d'autres compétences au besoin
     ];
 
@@ -21,8 +21,6 @@ document.addEventListener('DOMContentLoaded', function() {
             const skillName = this.dataset.skill;
             console.log(skillName);
             const skill = skills.find(skill => skill.name === skillName);
-            // const skill = skills.find();
-            // Bloquer le fond du site
             document.body.style.overflow = 'hidden';
             const modal = document.createElement('div');
             modal.classList.add('modal','active');
@@ -45,8 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         <span class="progress-percent">${skill.level}%</span>
                     </div>
                     <div class="card-content">
-                      <h1 class="card-title">Title</h1>
-                      <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                      <p class="card-text">${skill.description}</p>
                    </div>
                   </div>
             </div>
